9803c5254e9aa63a4516cc01f2a289e35bfb4b46
Mark Hallap <hallapmark@gmail.com>
2025-10-29 13:56:46 +0200

    Base functionality for loan payment calculator

--- src/components/LaenuKalkulaator.jsx
+++ src/components/LaenuKalkulaator.jsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 function LaenuKalkulaator() {
   const [kuumakse, setKuumakse] = useState(351.61);
   const [laenusumma, setLaenusumma] = useState(75000);
+  const [intressKokku, setIntressKokku] = useState(3.85);
 
   const ostuhindRef = useRef();
   const sissemakseRef = useRef();
@@ -11,10 +12,17 @@ function LaenuKalkulaator() {
   const euriborRef = useRef();
 
   function arvutaKuumakse() {
-    setLaenusumma(ostuhindRef.current.value - sissemakseRef.current.value)
+    const intress = Number(euriborRef.current.value) + Number(marginaalRef.current.value)
+    setIntressKokku(intress);
+    // current.vale on alati string
+
+    setLaenusumma(ostuhindRef.current.value - sissemakseRef.current.value);
 
     setKuumakse((ostuhindRef.current.value - sissemakseRef.current.value)
-    / 12 / perioodRef.current.value);
+    / 12 / perioodRef.current.value * intress / 2.3); // jäägilt tuleb, praegu lihtsalt hardcoded
+    // setid kogutakse funktsiooni lõpuks kokku ja siis alles käivitatakse. 
+    // setKuumakse() ajaks pole setIntressKokku veel seatud. nii et intressKokku ei
+    // saa kasutada.
   }
 
   return (
@@ -26,12 +34,24 @@ function LaenuKalkulaator() {
       <label>Laenusumma</label>
       <input value={laenusumma} disabled type="text" /><br />
       <label>Periood</label>
-      <input defaultValue={30} ref={perioodRef} onChange={arvutaKuumakse} type="text" /><br />
+      {/* <input defaultValue={30} ref={perioodRef} onChange={arvutaKuumakse} type="text" /><br /> */}
+      <select defaultValue={30} ref={perioodRef} onChange={arvutaKuumakse}>
+        <option value={5}>viis</option>
+        <option value={10}>kümme</option>
+        <option>15</option>
+        <option>20</option>
+        <option>25</option>
+        <option>30</option>
+        {/* {[...Array(30).keys()].map(option => 
+          <option key={option}>{option+1}</option>
+        )} */}
+      </select> <br />
       <label>Marginaal</label>
-      <input ref={marginaalRef} onChange={arvutaKuumakse} type="text" /><br />
+      <input defaultValue={1.7} ref={marginaalRef} onChange={arvutaKuumakse} type="number" /><br />
       <label>Euribor</label>
-      <input ref={euriborRef} onChange={arvutaKuumakse} type="text" /><br />
+      <input defaultValue={2.15} ref={euriborRef} onChange={arvutaKuumakse} type="number" /><br />
       <label>Intress kokku</label>
+      <input value={intressKokku.toFixed(2)} disabled type="text" /> <br />
       <input disabled type="text" /><br />
       { laenusumma < 10000000 && laenusumma > 20000 ?
         <div>Kuumakse {kuumakse.toFixed(2)}€</div> :
