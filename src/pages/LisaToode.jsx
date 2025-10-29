0bff74b68d3a4331511b21164736783ef6e05b7d
Mark Hallap <hallapmark@gmail.com>
2025-10-29 14:14:06 +0200

    Toast added product with react-hot-toast

--- src/pages/LisaToode.jsx
+++ src/pages/LisaToode.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { Toaster, toast } from 'react-hot-toast';
 
 function LisaToode() {
   const [sonum, setSonum] = useState("Lisa uus toode");
@@ -9,11 +10,13 @@ function LisaToode() {
       setSonum("Tühja nimega ei saa toodet lisada!");
     } else {
       setSonum("Toode lisatud!");
+      toast.success("Ostukorv on uuendatud", { duration: 2500 })
     }
   }
 
   return (
     <div>
+      <div><Toaster/></div>
       <div>{sonum}</div>
       <label>Nimi</label> <br/>
       <input ref={nimiRef} type="text" /> <br/>
