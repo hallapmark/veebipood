3c31f3b0d0ae902470a4ec3ccae33cee033ee857
Mark Hallap <hallapmark@gmail.com>
2025-11-02 19:25:04 +0200

    Flesh out products page

--- src/pages/arrays/Tooted.jsx
+++ src/pages/arrays/Tooted.jsx
@@ -1,6 +1,112 @@
+import { useState } from "react"
+import tootedAndmebaasist from "../../data/tooted.json"
+import styles from "../../css/tooted.module.css"
+
 function Tooted() {
+  const [tooted, setTooted] = useState(tootedAndmebaasist.slice());
+  // nb see on shallow copy  
+  // süvakoopiat vist ei tahagi siin? 
+  // vt https://developer.mozilla.org/en-US/docs/Web/API/Window/structuredClone
+  
+
+  function sorteeriAZ() {
+    tooted.sort((a,b) => a.name.localeCompare(b.name));
+    setTooted(tooted.slice());
+  }
+
+  function sorteeriZA() {
+    tooted.sort((a,b) => b.name.localeCompare(a.name));
+    setTooted(tooted.slice());
+  }
+
+  function sorteeriT2htedeArvAsc() {
+    tooted.sort((a,b) => a.name.length - b.name.length);
+    setTooted(tooted.slice());
+  }
+
+  function sorteeriT2htedeArvDesc() {
+    tooted.sort((a,b) => b.name.length - a.name.length);
+    setTooted(tooted.slice());
+  }
+
+  function sorteeriTeineTahtAZ() {
+    tooted.sort((a,b) => {
+      if (a.name.length >= 2 && b.name.length >= 2) {
+        return a.name[1].localeCompare(b.name[1]);
+      }
+      return 0;
+    });
+    setTooted(tooted.slice());
+  }
+
+  function filtreeriMillelOnTapselt6() {
+    const vastus = tooted.filter(toode => toode.name.length === 6);
+    setTooted(vastus);
+  }
+
+  function filtreeriMillelOnKuni6() {
+    const vastus = tooted.filter(toode => toode.name.length <= 6);
+    setTooted(vastus);
+  }
+
+  function filtreeriViimaneA() {
+    const vastus = tooted.filter(toode => toode[toode.length - 1].toLocaleLowerCase() === "a");
+    setTooted(vastus);
+  }
+
+  function filtreeriViimaneY() {
+    const vastus = tooted.filter(toode => toode[toode.length - 1].toLocaleLowerCase() === "y");
+    setTooted(vastus);
+  }
+
+  function filtreeriPaarisarvTahti() {
+    const vastus = tooted.filter(toode => {
+      const strNoSpaces = toode.name.replace(/\s+/g, '');
+      return strNoSpaces.length % 2 === 0;
+    });
+    setTooted(vastus);
+  }
+
+  function reset() {
+    setTooted(tootedAndmebaasist);
+  }
+
   return (
-    <div>Tooted</div>
+    <>
+      <h1>Tooted</h1>
+      <p>Meil on {tootedAndmebaasist.length} toodet</p>
+      <button onClick={reset}>Reset</button>
+      <h2>Sorteeri</h2>
+      <div style = {{ display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
+        <button onClick={sorteeriAZ}>AZ</button>
+        <button onClick={sorteeriZA}>ZA</button>
+        <button onClick={sorteeriT2htedeArvAsc}>Tähtede arv kasvavalt</button>
+        <button onClick={sorteeriT2htedeArvDesc}>Tähtede arv kahanevalt</button>
+        <button onClick={sorteeriTeineTahtAZ}>Teine täht AZ</button>
+      </div>
+      <h2>Filtreeri</h2>
+      <div style = {{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+        <button onClick={filtreeriMillelOnTapselt6}>Täpselt 6 tähte</button>
+        <button onClick={filtreeriMillelOnKuni6}>Kuni 6 tähte</button>
+        <button onClick={filtreeriViimaneA}>Viimane täht "a"</button>
+        <button onClick={filtreeriViimaneY}>Viimane täht "y"</button>
+      </div>
+      <div style = {{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+        <button onClick={filtreeriPaarisarvTahti}>Terves tootenimes paarisarv tähti</button>
+      </div>
+
+      <p>Kuvatud on {tooted.length} toodet</p>
+
+      <div>
+        {tooted.map(toode => 
+            <div className={styles.toode} key={toode.name}>
+              <h3>{toode.name}</h3>
+              <p>{toode.description_est}</p>
+              <p>{toode.price}€</p>
+            </div>
+        )}
+      </div>
+    </>
   )
 }
 
