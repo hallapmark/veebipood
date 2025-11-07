8b856fd0fc7ee74501e09a9486a16ea784f4954e
Mark Hallap <hallapmark@gmail.com>
2025-11-07 19:35:56 +0200

    Practice useNavigate

--- src/pages/muuda/MuudaAuto.jsx
+++ src/pages/muuda/MuudaAuto.jsx
@@ -1,20 +1,53 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import autoDB from "../../data/autod.json"
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function MuudaAuto() {
   const { index } = useParams();
-  const leitud = autoDB[index];
+  const [car, setCar] = useState(autoDB[index]);
+  const navigate = useNavigate(); 
+  // Reacti hook (Reacti erikood) 
+  // 1. peab algama use- eesliidesega
+  // 2. kõik use-id peavad olema top-levelil. 
+  // 3. ei tohi olla conditional/loodud tingimuslikult (if või early return)
+  // 4. peab olema imporditud
+  // 5. peab olema funktsionaalne ehk teda peab käivitama (sulgude abil)
 
-  if (leitud === undefined) {
+  function update() {
+    if (car.price <= 0) {
+      toast.error("Hind ei saa olla null või negatiivne");
+      return;
+    }
+    autoDB[index] = car;
+    navigate("/halda-autod");
+  }
+
+  if (car === undefined) {
     return <div>Autot ei leitud!</div>
   }  
 
   return (
     <div>
-      <input defaultValue={leitud.name} type="text" /> <br />
-      <input defaultValue={leitud.price} type="text" /> <br />
-      <input defaultValue={leitud.active} type="text" /> <br />
-      <input defaultValue={leitud.image} type="text" /> <br />
+      <label>Name</label> <br />
+      <input defaultValue={car.name} onChange={(e) => setCar({...car, name: e.target.value})} type="text" /> <br />
+      <label>Price</label> <br />
+      <input defaultValue={car.price} onChange={(e) => setCar({...car, price: Number(e.target.value)})} type="number" /> <br />
+      <label>Active</label> <br />
+      <input defaultValue={car.active} onChange={(e) => setCar({...car, active: e.target.checked})} type="checkbox" /> <br />
+      <label>Image</label> <br />
+      <input defaultValue={car.image} onChange={(e) => setCar({...car, image: e.target.value})} type="text" /> <br />
+      {/* <Link to="/halda-autod"> */}
+        <button onClick={update}>Change</button>
+      {/* </Link> */}
+      {/* reacti muud erikoodi vaja siin navigeerimiseks. Me ei taha et alati link toimiks, ainult siis kui success */}
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        theme="dark"
+      />
+      
     </div>
   )
 }
--- src/pages/muuda/MuudaEsindus.jsx
+++ src/pages/muuda/MuudaEsindus.jsx
@@ -1,19 +1,34 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import esindusedDB from "../../data/esindused.json"
+import { useState } from "react";
 
 function MuudaEsindus() {
   const { index } = useParams();
-  const leitud = esindusedDB[index];
+  const [shop, setShop] = useState(esindusedDB[index]);
+  const navigate = useNavigate();
 
-  if (leitud === undefined) {
+  function change() {
+    if (shop.name === "") {
+      alert("Tühja nimega ei saa poodi sisestada!");
+      return;
+    }
+    esindusedDB[index] = shop;
+    navigate("/halda-esindused");
+  }
+
+  if (shop === undefined) {
     return <div>Esindust ei leitud!</div>
   }  
 
   return (
     <div>
-      <input defaultValue={leitud.name} type="text" /> <br />
-      <input defaultValue={leitud.telefon} type="text" /> <br />
-      <input defaultValue={leitud.aadress} type="text" /> <br />
+      <label>Name</label> <br />
+      <input defaultValue={shop.name} onChange={(e) => setShop({...shop, name: e.target.value})} type="text" /> <br />
+      <label>Telephone</label> <br />
+      <input defaultValue={shop.telefon} onChange={(e) => setShop({...shop, telefon: e.target.value})} type="text" /> <br />
+      <label>Address</label> <br />
+      <input defaultValue={shop.aadress} onChange={(e) => setShop({...shop, aadress: e.target.value})} type="text" /> <br />
+      <button onClick={change}>Change</button>
     </div>
   )
 }
