0954ec8b8ee0b2a7271cde3f13b2fda36b47f263
Mark Hallap <hallapmark@gmail.com>
2025-11-12 20:43:18 +0200

    Add ability to edit products

--- src/pages/halda/HaldaTooted.jsx
+++ src/pages/halda/HaldaTooted.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import tootedAndmebaasist from "../../data/tooted.json"
 import { ToastContainer, toast } from "react-toastify";
+import { Link } from "react-router-dom";
 // import styles from "../../css/Haldatooted.module.css"
 
 function HaldaTooted() {
 
-  const [tooded, setTooted] = useState(tootedAndmebaasist.slice());
+  const [tooted, setTooted] = useState(tootedAndmebaasist.slice());
 
   function kustuta(index) {
     tootedAndmebaasist.splice(index,1);
@@ -25,12 +26,17 @@ function HaldaTooted() {
           </tr>
         </thead>
         <tbody>
-          {tooded.map((toode, index) => 
+          {tooted.map((toode, index) => 
             <tr key={toode.name}>
               <td>{index+1}</td>
               <td>{toode.name}</td>
               <td>
                 <button onClick={() => kustuta(index)}>X</button>
+              </td>
+              <td>
+                <Link to={`/muuda-toode/${index}`}>
+                  <button>Muuda</button>
+                </Link>
               </td>
             </tr>
           )}
--- src/pages/muuda/MuudaToode.jsx
+++ src/pages/muuda/MuudaToode.jsx
@@ -1,7 +1,46 @@
+import { useNavigate, useParams } from "react-router-dom"
+import tooteDB from "../../data/tooted.json"
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function MuudaToode() {
+  const { index } = useParams();
+  const [toode, setToode] = useState(tooteDB[index]);
+  const navigate = useNavigate(); 
+
+  function update() {
+    if (toode.name.length < 2) {
+      toast.error("Toote nimi on liiga lühike!");
+      return;
+    }
+
+    tooteDB[index] = toode;
+    navigate("/halda-tooted");
+  }
+
+  if (toode === undefined) {
+    return <div>Toodet ei leitud!</div>
+  }  
+
   return (
-    <div>MuudaToode</div>
+    <div>
+      <label>Name</label> <br />
+      <input defaultValue={toode.name} onChange={(e) => setToode({...toode, name: e.target.value})} type="text" /> <br />
+      <label>Description (en)</label> <br />
+      <input defaultValue={toode.description} onChange={(e) => setToode({...toode, description: e.target.value})} type="text" /> <br />
+      <label>Description (est)</label> <br />
+      <input defaultValue={toode.description_est} onChange={(e) => setToode({...toode, description_est: e.target.value})} type="text" /> <br />
+      <label>Price</label> <br />
+      <input defaultValue={toode.price} onChange={(e) => setToode({...toode, price: Number(e.target.value)})} type="number" /> <br />
+      <button onClick={update}>Change</button>
+
+      <ToastContainer
+        position="bottom-right"
+        toodeClose={4000}
+        theme="dark"
+      />
+      
+    </div>
   )
 }
 
