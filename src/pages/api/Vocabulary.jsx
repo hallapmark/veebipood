79efb5b2d72ea728321e4249c4b87303a2e49f54
Mark Hallap <hallapmark@gmail.com>
2025-11-13 16:10:01 +0200

    Display a list of marine vocabulary definitions

--- src/pages/api/Vocabulary.jsx
+++ src/pages/api/Vocabulary.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+
 function Vocabulary() {
+  const [totalDefinitions, setTotalDefinitions] = useState(0);
+  const [definitions, setDefinitions] = useState([]);
+  const [spinner, setSpinner] = useState(false)
+
+  useEffect(() => {
+    setSpinner(true);
+    fetch("https://marineregions.org/rest/getGazetteerTypes.json")
+      .then(r => r.json())
+      .then(json => {
+        setDefinitions(json);
+        setTotalDefinitions(json.length);
+        setSpinner(false);
+      });
+  }, []);
+
   return (
-    <div>Vocabulary</div>
+    <div>
+      <div>Marine regions dictionary</div> <br />
+      {!spinner ? <div>Total number of definitions: {totalDefinitions}</div> :(
+        <div>
+          <div>Loading definitions ...</div>
+          <div className="dots">
+            <div></div><div></div><div></div>
+          </div>
+        </div>
+      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Word</th>
+            <th>Definition</th>
+          </tr>
+        </thead>
+        <tbody>
+          {definitions.map(definition => 
+            <tr key={definition.typeID}>
+              <td>{definition.type}</td>
+              <td>{definition.description}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 export default Vocabulary
+
 
 // https://marineregions.org/rest/getGazetteerTypes.json
